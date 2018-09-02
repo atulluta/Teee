bf29c96bf46a39d74370f3aaf8f7c8594ed3709f
atul123 <abharadwajj@gmail.com>
2018-09-02 20:50:43 +0530

    new class e2

---
+++ Teee/src/Pages/E2.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class E2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sade wade");
+System.out.println("Sade wade sade");
+	}
+
+}
