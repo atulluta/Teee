1b3133a97fabb5f853864c051a90be41ddad5ec4
atulb123 <30617666+atulb123@users.noreply.github.com>
2018-09-02 22:05:31 +0530

    Update E.java

--- Teee/src/Pages/E.java
+++ Teee/src/Pages/E.java
@@ -5,7 +5,7 @@ public class E {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Sade wade");
-System.out.println("Sade wade sade");
+System.out.println("Sade wade sade halkat");
 System.out.println("macd");
 	}
 
